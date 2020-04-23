09590a02a0983525fa2a68a41c1504f3ce910923
dianakw8591 <dianakw@gmail.com>
2020-04-23 12:59:38 -0700

    modified associations on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_many :user_climbs
-  has_many :climbs, through: :user_climbs, uniq: :true
-  has_many :locations, through: climbs, uniq: :true
+  has_many :climbs, -> { distinct }, through: :user_climbs
+  has_many :areas, -> { distinct }, through: :climbs
 end
