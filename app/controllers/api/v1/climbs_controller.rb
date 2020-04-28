b044196b8ec828bceb5cebdc4f6e6fca868a7084
dianakw8591 <dianakw@gmail.com>
2020-04-28 11:01:59 -0700

    limited number of climbs returned

--- app/controllers/api/v1/climbs_controller.rb
+++ app/controllers/api/v1/climbs_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::ClimbsController < ApplicationController
   def index
     @q = Climb.ransack({name_i_cont: params[:q]})
-    climbs = @q.result(distinct: true)
+    climbs = @q.result(distinct: true).limit(8)
     render json: {climbs: climbs}
   end
 end
